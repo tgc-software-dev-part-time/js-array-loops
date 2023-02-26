3293ed4c84e1a164d17a9213565aebd3012a2724
Edison Zhuang <edisen.zhuang@gmail.com>
2023-02-26 19:19:25 +0800

    answer to homework

---
+++ src/homework/code-challenge.js
@@ -0,0 +1,32 @@
+const readline = require('readline');
+
+const fruitStr = "apple orange mongo durian strawberry banana grapefruit raspberry";
+let fruits = fruitStr.split(" ");
+let input = "banana";
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Input a fruit? ', (userInput) => {
+    input = userInput;
+    rl.close();
+
+    let hasMatch = false; // Use to check if there is a match
+
+    for (let i = 0; i < fruits.length; i++) {
+        const currentFruit = fruits[i];
+        if (currentFruit.toLowerCase() === input.toLowerCase()) {
+            console.log("Congratulations!!");
+            hasMatch = true; // Set to true when there is a match
+            break;
+        }
+        console.log(currentFruit);
+    }
+
+    if (!hasMatch) console.log("Word has no match."); // Notify user when there is no match
+});
+
+
+
